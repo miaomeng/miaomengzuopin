552d9baf634f906b61c7b5a97aaacf4613226606
miaomeng <410732132@qq.com>
2016-12-22 00:32:22 +0800

    uuu

--- 20161220跨域问题/php/1.php
+++
@@ -1,5 +0,0 @@
-<?php
-//header('Access-Control-Allow-Origin:*');
-//header('content-type:text/html;charset=utf-8');
-
-echo 'hello';
--- 20161220跨域问题/php/2.get.php
+++
@@ -1,3 +0,0 @@
-<?php
-$ymjz = file_get_contents('http://www.kaola.com/getSuggestKeyword.html?query=&size=10');
-echo $ymjz;
